59c5fcdddc412f0901582f357f08ae0735fe6c3a
JangHun93 <pain3992@gmail.com>
2024-07-30 15:17:15 +0900

    First Initialize

---
+++ Source/TheGeniusPlan.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TheGeniusPlanTarget : TargetRules
+{
+	public TheGeniusPlanTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("TheGeniusPlan");
+	}
+}
