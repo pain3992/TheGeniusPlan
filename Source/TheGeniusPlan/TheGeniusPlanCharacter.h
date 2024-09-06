4723331e1fbe982e7775b7b87e4f1ae21bf881db
JangHun93 <pain3992@gmail.com>
2024-09-06 13:40:43 +0900

    TheGeniusPlanCharacter 네트워크 동기화 오류 테스트

--- Source/TheGeniusPlan/TheGeniusPlanCharacter.h
+++ Source/TheGeniusPlan/TheGeniusPlanCharacter.h
@@ -56,6 +56,8 @@ public:
 	UFUNCTION()
 	void changeCameraMode();
 
+	virtual void Tick(float DeltaSeconds) override;
+
 protected:
 	/** Called for movement input */
 	void Move(const FInputActionValue &Value);
